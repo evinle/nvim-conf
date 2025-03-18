28ea5cd273381357224dd7bed4bd1a9606ea3f46
Tuan Le <tuan.le@bhp.com>
2025-03-18 19:01:57 +0800

    fixed bug where <leader>gs was not opening split status view

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -12,7 +12,7 @@ return { {
             graph_style = "unicode",
         })
 
-        vim.keymap.set('n', '<leader>gs kind=vsplit', neogit.open, { silent = true, noremap = true })
+        vim.keymap.set('n', '<leader>gs', ":Neogit kind=vsplit<cr>", { silent = true, noremap = true })
         vim.keymap.set('n', '<leader>gc', ":Neogit commit<cr>", { silent = true, noremap = true })
         vim.keymap.set('n', '<leader>gp', ":Neogit pull<cr>", { silent = true, noremap = true })
         vim.keymap.set('n', '<leader>gP', ":Neogit push<cr>", { silent = true, noremap = true })
